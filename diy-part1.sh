9b373123ce2946d77baa8281150d8aec84075613
nfervor <nfervor@gmail.com>
2022-04-04 20:23:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,13 +23,11 @@ echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>feeds.conf.def
 
 
 
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/ #git lua-maxminddb 依赖
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/lienol
 git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
 # git clone -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
 # git clone https://github.com/pymumu/openwrt-smartdns package/openwrt-smartdns     
 # git clone https://github.com/Tencent-Cloud-Plugins/tencentcloud-openwrt-plugin-ddns package/luci-app-tencentddns 
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
-# git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome 
